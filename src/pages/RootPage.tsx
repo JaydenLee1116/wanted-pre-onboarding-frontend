68371436bc05359c2d889e27ed827dd37b5ba82c
JaydenLee1116 <vv55adss@gmail.com>
2023-07-31 18:34:01 +0900

    💄 design: Root page UI 구현

--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -1,5 +1,15 @@
+import Button from '../components/Button';
+
 const RootPage = () => {
-  return <div>루트 페이지</div>;
+  return (
+    <section className="flex h-96 w-full flex-col items-center justify-between">
+      <div className="text-4xl font-bold">JDN TODO LIST</div>
+      <section className="flex w-48 flex-row justify-between">
+        <Button onClick={() => {}}>회원가입</Button>
+        <Button onClick={() => {}}>로그인</Button>
+      </section>
+    </section>
+  );
 };
 
 export default RootPage;
