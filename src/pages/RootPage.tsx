69e29fb37c04156ea1b0736e621ec9ea60f2411e
JaydenLee1116 <vv55adss@gmail.com>
2023-07-31 18:39:35 +0900

    ✨ feat: Root page navigate 구현
    - 회원가입 페이지 이동
    - 로그인 페이지 이동

--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -1,12 +1,31 @@
+import { useNavigate } from 'react-router-dom';
+
 import Button from '../components/Button';
 
 const RootPage = () => {
+  const navigate = useNavigate();
+  const handleNavigateBtnClick = (path: string) => {
+    navigate(path);
+  };
+
   return (
     <section className="flex h-96 w-full flex-col items-center justify-between">
       <div className="text-4xl font-bold">JDN TODO LIST</div>
       <section className="flex w-48 flex-row justify-between">
-        <Button onClick={() => {}}>회원가입</Button>
-        <Button onClick={() => {}}>로그인</Button>
+        <Button
+          onClick={() => {
+            handleNavigateBtnClick('/signup');
+          }}
+        >
+          회원가입
+        </Button>
+        <Button
+          onClick={() => {
+            handleNavigateBtnClick('/signin');
+          }}
+        >
+          로그인
+        </Button>
       </section>
     </section>
   );
