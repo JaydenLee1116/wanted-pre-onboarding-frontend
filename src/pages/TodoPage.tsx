1476999482796258faaaea60a7767f7b89c2cdd2
JaydenLee1116 <vv55adss@gmail.com>
2023-08-04 17:04:52 +0900

    ✨ feat: 로컬스토리지 accessToken 존재 시, 투두에서 로그인으로 이동

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,4 +1,13 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const TodoPage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (!accessToken) navigate('/signin');
+  });
+
   return <div>투두 페이지입니다.</div>;
 };
 
